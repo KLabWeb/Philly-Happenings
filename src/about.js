864380906e3ec0d1edce62214eb3f927f9798c34
Lain Iwakura <klabweb@protonmail.com>
2023-01-23 08:10:08 -0500

    minor "about" update

--- src/about.js
+++ src/about.js
@@ -8,7 +8,7 @@ export const About = () => {
       <div className="aboutCard">
         <p>This site is a collection of DIY, art, music, and nature groups, spaces, and events in Philadelphia and the surrounding area. It gives a special focus to underground spaces, counter culture happenings, earthy delights, and music.</p>
 
-        <p>Want to add a spot, event, group, or resource to this site? Run a spot, event, group, or resource and don't like the description of it on here? Found an error on incorrect info? See something that is out of date and needs updating? Send an email to <a href="mailto: philly-happenings@protonmail.com">philly-happenings@protonmail.com</a> to have the site updated.</p>
+        <p>Want to add a spot, event, group, or resource to this site? Run a spot, event, group, or resource and want to change the description of it on here? Found an error or incorrect info? See something that is out of date and needs updating? Send an email to <a href="mailto: philly-happenings@protonmail.com">philly-happenings@protonmail.com</a> to have the site updated.</p>
 
         <p>This site is a work in progress and some sections have not been fully built out yet.</p>
       </div>
