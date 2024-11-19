f18a1f209f7f6ebfc31347f16d0bced680ae7cdd
Lain Iwakura <klabweb@protonmail.com>
2024-11-19 10:31:37 -0500

    quick about to About card

--- src/about.js
+++ src/about.js
@@ -8,11 +8,11 @@ export const About = () => {
       <div className="aboutCard">
         <p>A Yellow Pages for DIY, art, music, anarchist, and nature groups, spaces, and events in Philadelphia and the surrounding area. It gives a special focus to underground spaces, counter culture happenings, wild earth, and music.</p>
 
-        <p>Follow me on <a href="https://www.instagram.com/philly.happenings" alt="Insta" target="_blank" rel="noreferrer">Instagram</a> for site news</p>
+        <p>Follow me on <a href="https://www.instagram.com/philly.happenings" alt="Insta" target="_blank" rel="noreferrer">Instagram</a> for site news and updates.</p>
+        
+        <p>Send me a message on <a href="https://www.instagram.com/philly.happenings" alt="Insta" target="_blank" rel="noreferrer">Instagram</a> to have your space, venue, or group added to the site, or to have existing card details updated.</p>
 
-        <p>Send an email to <a href="mailto: philly-happenings@protonmail.com">philly-happenings@protonmail.com</a> to have your space, venue, or group added to the site.</p>
-
-        <p>I'll also post a video for local bands (music video, live recording, etc.) and local groups on the site, under the "Intermission" section. Send me a message.</p>
+        <p>I also post videos for local/regional bands (music videos, shows, etc.) and local groups on the site, under the <a href="#mid-page">Intermission</a> section. Send me a message on Insta to add your video.</p>
 
         <p>This site is a work in progress and some sections have not been fully built out yet.</p>
 
