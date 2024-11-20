1e8162fcc756c763c3af918edbaefbef7ff33b87
Lain Iwakura <klabweb@protonmail.com>
2024-11-20 10:14:54 -0500

    remove some FNB cook locations, and replace with [ask a punk], for better opsec for these FNB

--- src/data.js
+++ src/data.js
@@ -16,14 +16,14 @@ export const ANARCHISTPHILLY = [
   },
   {
     title: "West Philadelphia Food Not Bombs",
-    summary: "Serve Fridays 4:30pm at Malcolm X Park. Cook at Ahimsa House at 3:00pm. Food pickup for cook 8-9am (drivers needed).",
+    summary: "Serve Fridays 4:30pm at Malcolm X Park. Cook at [ask a punk] at 3:00pm. Food pickup for cook 8-9am (drivers needed).",
     facebook: "https://www.facebook.com/groups/westphillyfnb/",
     insta: "https://www.instagram.com/foodnotbombswestphilly/",
     suggestedFollow: "Insta (more updated)",
   },
   {
     title: "South Philadelphia Food Not Bombs",
-    summary: "“Saturdays Hot Meals @ 3pm. Fridays Fresh Produce @ 8am. Mifflin Square Park (Ritner and 5th).“ Cook at People's Kitchen. Very active.",
+    summary: "“Saturdays Hot Meals @ 3pm. Fridays Fresh Produce @ 8am. Mifflin Square Park (Ritner and 5th).“ Cook at [ask a punk]. Very active.",
     facebook: "https://www.facebook.com/groups/184708858232748/",
     insta: "https://www.instagram.com/southphillyfoodnotbombs/",
     suggestedFollow: "None (social media not updated but this FnB is very active, despite)",
