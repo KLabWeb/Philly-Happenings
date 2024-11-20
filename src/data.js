fd51c28b68257e55ee2d83cd0b6be9f49b14cb37
Lain Iwakura <klabweb@protonmail.com>
2024-11-20 12:23:36 -0500

    drop S Philly FNB insta, which is not insta for current S Philly FNB

--- src/data.js
+++ src/data.js
@@ -25,7 +25,6 @@ export const ANARCHISTPHILLY = [
     title: "South Philadelphia Food Not Bombs",
     summary: "“Saturdays Hot Meals @ 3pm. Fridays Fresh Produce @ 8am. Mifflin Square Park (Ritner and 5th).“ Very active. Food pickup drivers needed.",
     facebook: "https://www.facebook.com/groups/184708858232748/",
-    insta: "https://www.instagram.com/southphillyfoodnotbombs/",
     suggestedFollow: "None (social media not updated but this FnB is very active, despite)",
   },
   {
